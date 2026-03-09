4dec6b64686f16c0b97fb151f1a763cfa8266424
Hedge <46279228+HedgeWizardly@users.noreply.github.com>
2026-03-09 16:45:00 +0000

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -61,13 +61,7 @@ function createChildWindow(str:string): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    childWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-  } else {
-    childWindow.loadFile(join(__dirname, '../renderer/index.html'))
-  }
+  childWindow.loadFile(join(__dirname, '../renderer/index.html'))
 }
 
 // This method will be called when Electron has finished
